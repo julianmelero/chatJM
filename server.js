fd7a8c1512aaffb7a3ba9742dd5dfe7607ba057e
Juliánn Melero Hidalgo <julian@melerohidalgo.es>
2020-10-21 10:12:14 +0200

    Usamos Body parser de express

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 //Módulos
 const express = require('express');
 const router = express.Router();
+// Body Parser: permite trabajar con el body
+// YA VIENE CON EXPRESS INCLUIDO :D
+//const bodyParser = require('body-parser');
+
+
+
 
 var app = express();
 
-// Usamos las rutas
+// Usamos los módulos
+app.use(express.json());
+//app.use(bodyParser.urlencoded( { extended: false}));
+app.use(express.urlencoded( { extended: true }));
+//Ponemos el Router el último, ya que si no, no funciona el BodyParser
 app.use(router);
 
-// Aplicamos Rutas
-router.get('/', function (req,res) {
+
+// Aplicamos Rutas con sus métodos
+router.get('/', function (req,res) {    
     res.send('Lista de mensajes');
 });
 
 router.post('/message', function (req,res) {
-    res.send('Mensaje añadido');
+    console.log(req.body);
+    console.log(req.query);
+    res.send('Mensaje ' + req.body.mensaje /* mensaje = nombre del valor en url-encoded */ + ' añadido');
 
 });
 
