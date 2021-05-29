45966650d0991f140c5b8a25c425d58f53d324ba
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-05-29 18:41:34 +0200

    WebSocket

--- Components/message/controller.js
+++ Components/message/controller.js
@@ -1,83 +1,83 @@
-const store = require('./store');
+const { socket } = require("../../socket");
+const store = require("./store");
+const socketcon = require("../../socket").socket;
 
+function addMessage(user, message, message2, file) {
+  return new Promise((resolve, reject) => {
+    if (!user || !message) {
+      console.error("[messageController] No hay usuario o mensaje");
+      reject("Datos incorrectos");
+    }
+    let fileurl = "";
 
+    if (file) {
+      fileurl = "http://localhost:3000/app/files/" + file.filename;
+    }
+    const fullMessage = {
+      user: user,
+      message: message,
+      date: new Date(),
+      message2: message2,
+      file: fileurl,
+    };
 
-function addMessage(user, message , message2, file) {    
-    return new Promise ((resolve, reject) => {
-        if(!user || !message) {            
-            console.error('[messageController] No hay usuario o mensaje');
-            reject('Datos incorrectos');            
-        }
-        let fileurl = "";
+    store.add(fullMessage);
 
-        if(file) {
-            fileurl = "http://localhost:3000/app/files/" + file.filename;
-        }
-        const fullMessage = {
-            user : user,
-            message: message,
-            date: new Date,
-            message2: message2,
-            file: fileurl,
-        };
-        
-        store.add(fullMessage);
+    // Mandamos mensaje desde el socket
+    socketcon.io.emit("message", fullMessage);
 
-        resolve(fullMessage);
-        
-    });
-   
+    resolve(fullMessage);
+  });
 }
 
 function getMessages(filterMessages) {
-    return new Promise( (resolve,reject) => {
-        resolve(store.list(filterMessages));
-    });
+  return new Promise((resolve, reject) => {
+    resolve(store.list(filterMessages));
+  });
 }
 
-function getUserMessage(user) {    
-    return new Promise( (resolve,reject) => {
-        if(!user) {
-            reject('Invalid data'); 
-            return false;           
-        }
-        resolve(store.getUserMessage(user));
-    });
+function getUserMessage(user) {
+  return new Promise((resolve, reject) => {
+    if (!user) {
+      reject("Invalid data");
+      return false;
+    }
+    resolve(store.getUserMessage(user));
+  });
 }
 
-
-async function updateMessage(id,message) {
-    return new Promise(  (resolve,reject) => {        
-        if(!id || !message) {
-            reject('Invalid data'); 
-            return false;           
-        }
-        const result =  store.updateText(id,message);
-        resolve(result);
-    });
+async function updateMessage(id, message) {
+  return new Promise((resolve, reject) => {
+    if (!id || !message) {
+      reject("Invalid data");
+      return false;
+    }
+    const result = store.updateText(id, message);
+    resolve(result);
+  });
 }
 
 async function deleteMessage(id) {
-    return new Promise ( (resolve,reject) => {
-        if(!id) {
-            reject('ID inválido');
-            return false;
-        }
-        store.remove(id)
-        .then( () => {
-            resolve();
-        })
-        .catch(e => {
-            reject(e);
-        })
-
-    })
+  return new Promise((resolve, reject) => {
+    if (!id) {
+      reject("ID inválido");
+      return false;
+    }
+    store
+      .remove(id)
+      .then(() => {
+        resolve();
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
 }
 
 module.exports = {
-    addMessage,
-    getMessages,
-    updateMessage,
-    getUserMessage,
-    deleteMessage,
+  addMessage,
+  getMessages,
+  updateMessage,
+  getUserMessage,
+  deleteMessage,
 };
--- server.js
+++ server.js
@@ -1,29 +1,41 @@
 //Módulos
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+var app = express();
+const server = require('http').Server(app);
+const cors = require("cors");
+
+const socket = require('./socket');
+
 
 
 // Llamamos a conexión DB
-const db = require('./db');
+const db = require("./db");
 
 // const router = require('./Components/message/network');
-const router = require('./network/routes') ;
+const router = require("./network/routes");
 // Body Parser: permite trabajar con el body
 // YA VIENE CON EXPRESS INCLUIDO :D
 //const bodyParser = require('body-parser');
 
-const keys = require('./keys');
+const keys = require("./keys");
 
-db("mongodb+srv://" + USER + ":" + PASS + "@" + CLUSTER + "/<" + BD +">?retryWrites=true&w=majority");
+db(
+  "mongodb+srv://" +
+    USER +
+    ":" +
+    PASS +
+    "@" +
+    CLUSTER +
+    "/<" +
+    BD +
+    ">?retryWrites=true&w=majority"
+);
 
-
-
-var app = express();
 app.use(cors());
 // Usamos los módulos
 app.use(express.json());
 //app.use(bodyParser.urlencoded( { extended: false}));
-app.use(express.urlencoded( { extended: true }));
+app.use(express.urlencoded({ extended: true }));
 //Ponemos el Router el último, ya que si no, no funciona el BodyParser
 // app.use(router);
 /*
@@ -31,9 +43,10 @@ Envío app ya que lo que haré es usar su función "use"
 */
 router(app);
 
+socket.connect(server);
 // app.use('/app', express.static('public'));
 
+server.listen(3000, function () {
+    console.log("Escuchando en puerto 3000");
+});
 
-
-app.listen(3000);
-console.log('Escuchando en puerto 3000');
